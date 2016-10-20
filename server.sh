ba0915795ac49ff3d41bd35ae4a90c45d6f9a111
Elisha Lai <elisha.lai@outlook.com>
2016-10-20 17:11:56 -0400

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##==============================================================================
+## Introductory Socket Programming
+##
+## @description: Run script for server.java
+## @author: Elisha Lai
+## @version: 1.0 10/11/2016
+##==============================================================================
+
+# Number of Parameters: 1
+# Parameter:
+#    $1: <req_code>
+java server "$@" 

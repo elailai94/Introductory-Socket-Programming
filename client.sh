11c0c863f7739531de134700a68c1fb71bf0e5a7
Elisha Lai <elisha.lai@outlook.com>
2016-10-20 17:10:29 -0400

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##==============================================================================
+## Introductory Socket Programming
+##
+## @description: Run script for client.java
+## @author: Elisha Lai
+## @version: 1.0 10/11/2016
+##==============================================================================
+
+# Number of Parameters: 4
+# Parameter:
+#    $1: <server_address>
+#    $2: <n_port>
+#    $3: <req_code>
+#    $4: <msg>
+java client "$@"

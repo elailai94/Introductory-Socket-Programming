8c3b5ae3ba24fdb90aa26158c13cecf919bbf75a
Elisha Lai <elisha.lai@outlook.com>
2016-12-16 03:54:17 -0500

    minor changes

--- source-code/server.java
+++ source-code/server.java
@@ -3,7 +3,7 @@
 //
 // @description: An implementation of a server program in Java
 // @author: Elisha Lai
-// @version: 1.0 11/10/2016
+// @version: 1.0 10/11/2016
 //==============================================================================
 
 import java.io.BufferedReader;
@@ -17,7 +17,7 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 
 public class server {
-      public static void main(String[] args) throws Exception {
+   public static void main(String[] args) throws Exception {
       // Checks the number and formats of the command line arguments passed
       checkCommandLineArguments(args);
 
